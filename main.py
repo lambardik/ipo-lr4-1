6b087b874a6beba60540b880d98f43240b50f26a
даниэль <dannilboyar@gmail.com>
2024-10-24 09:31:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+def print_even_numbers(number):
+  print("Четные числа от 0 до", number, ":", end=" ")
+  for i in range(0, number + 1, 2):
+    print(i, end=" ")
 number = int(input("Введите число: "))
-print("Четные числа от 0 до", number, ":", end=" ")
-for i in range(0, number + 1, 2):
-  print(i, end=" ")
+print_even_numbers(number)
